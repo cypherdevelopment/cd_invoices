3ce0b3c1912e1d9171d5afc3e9df4034735e3eff
DevKayne <78748546+DevKayne@users.noreply.github.com>
2023-06-03 16:27:36 +0800

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,30 +1,19 @@
-local tabOpen = false 
-
--- Command to Open -- 
-RegisterCommand(Config.commandName, function()
-    TriggerEvent('cd_businesstab:usetablet')
+RegisterNetEvent('openinvoice')
+AddEventHandler('openinvoice', function()
+    SendNUIMessage({
+        type = "createinvoice"
+    })
+    SetNuiFocus(false, false)
 end)
 
--- UI Event -- 
-RegisterNetEvent('cd_businesstab:usetablet')
-AddEventHandler('cd_businesstab:usetablet', function()
-    if not tabOpen then 
-        SendNUIMessage({
-            type = "opentablet"
-        })
-        tabOpen = true 
-        SetNuiFocus(true, true)
-    else 
-        SendNUIMessage({
-            type = "closetablet"
-        })
-        tabOpen = false 
-        SetNuiFocus(false, false)
-    end
+RegisterCommand("openui", function()
+    TriggerEvent('openinvoice')
+    SetNuiFocus(true,true)
 end)
 
--- NUI Callbacks --
-RegisterNUICallback('cd_businesstab:closebtn', function(_, cb) 
+RegisterNuiCallback('closeinvoice', function(_, cb)
     cb({})
     SetNuiFocus(false, false)
 end)
+
+RegisterKeyMapping('openui', "test", 'keyboard', 'F5')
